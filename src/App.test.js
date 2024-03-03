fcec622fe08deed1d6fa391cd64303a0af88b0cf
KennyCandra <142558663+KennyCandra@users.noreply.github.com>
2024-03-03 02:24:09 +0200

    form and form validation

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+// import App from './App';
+import BookingForm from './Components/BookingForm';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+  test('Check if this Text is avaliable in the screen', () => {
+    render(<BookingForm />);
+    const headingElement = screen.getByText('Choose date');
+
+    expect(headingElement).toBeInTheDocument();
+  })
